046db6d8adf53c3b7e996bc9ab96b173eeb1c7f4
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-05 12:07:47 +0530

    make header

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+const Header: React.FC = () => {
+  return (
+    <h1 className="text-5xl font-bold text-white capitalize font-roboto">
+      happy birthday <span className="text-pink-300">rachey!</span>
+    </h1>
+  );
+};
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 background"></div>
-  )
-}
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 background">
+      <Header />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
